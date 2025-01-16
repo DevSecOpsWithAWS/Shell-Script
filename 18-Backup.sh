9011b96aa8d0c9b942ea03488cd8239b5793d040
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-01-16 10:52:02 +0530

    18-Backup.sh

---
+++ 18-Backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+SOURCE_DIR=$1
+DIST_DIR=$2
+DAYS=${3:-14} # Days is a optional parameter if user not given any value then by default it will take 14 Days
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOG_FOLDER_NAME="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d '.' -f1)
+TIMESTAMP=$(date +%Y-%m-%d::%H:%M:%S)
+LOG_FILE_NAME="$LOG_FOLDER_NAME/$LOG_FILE-$TIMESTAMP.log"
+
+mkdir -p /home/ec2-user/shellscript-logs
+USAGE(){
+  echo -e "USAGE:: backup <SOURCE_DIR> <DIST_DIR> <DAYS(Optional)>"
+  exit 1
+}
+
+if [ $# -lt 2]
+then
+  USAGE
+done
