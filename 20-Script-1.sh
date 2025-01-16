af87cd11d31bf1834e0671a96182d1de48dd69e0
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-01-16 12:33:56 +0530

    20-Script-1.sh and 21-Script-2.sh

---
+++ 20-Script-1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+MSG="SCRIPT-1"
+GREET="Hi From SCRIPT-1"
+source ./21-Script-2.sh
+echo "Hello from: $MSG"
+echo "A value: $A"
---
+++ 21-Script-2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+MSG="SCRIPT-2"
+A="10"
+
+echo "Hello from: $MSG"
+
+echo "Greetings from: $GREET"
